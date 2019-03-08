dad65c50db092316b4f01f24e1fa0ccb37401708
improbablyalive <firewolf1324@gmail.com>
2019-03-08 16:58:10 -0500

    edits to Intcoll6
    forgot part of omit
    added in comments
    
    noticed a weird aspect in tests, sometimes fails when client uses Intcoll6(2) at the start and the first value entered is negative or a series like 23,24,-23 is put in

--- Intcoll6.java
+++ Intcoll6.java
@@ -1,29 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Intcoll6.java 
+ * this file contains the class Intcoll, a collection of ints
+ * 
+ *  @version  20190308
+ *  @author   Athena Evans
  */
-package Assignment4;
 
-/**
- *
- * @author Merisa
- */
 public class Intcoll6 {
-   private int howMany;
-   private btNode c;
-
-   public Intcoll6(){
+   private int howMany;          // c refers to the collection
+   private btNode c;             // howMany is the collection count
+   
+   public Intcoll6(){            // instantiation of Intcoll6, no param
       c = null;
       howMany = 0;
    }
 
-   public Intcoll6(int i){
+   public Intcoll6(int i){       // instantiation of Intcoll6, int param i is for consistency
       c = null;
       howMany = 0;
    }
     
-    public void print(){
+    public void print(){         //formats the info of the collection and prints as a series of strings
+                                 //as a void function print() returns nothing
         printtree(c);
     }
     private static void printtree(btNode t){
@@ -33,8 +30,19 @@ public class Intcoll6 {
             printtree(t.right);
         }
     }
-
-    private static btNode copytree(btNode t){
+    
+    public void copy(Intcoll6 obj){       //makes a copy of another Intcoll object and if they are the same then copy does
+                                          //nothing. If it does not match then copy replaces this Intcoll object with the 
+                                          //copy. REQ ANOTHER Intcoll obj.
+                                          //example --> a.copy(b) will make it so that a contains the same array as b
+                                          //as a void function copy(Intcoll4 obj) returns nothing
+        if (this!=obj){
+            this.howMany=obj.howMany;
+            c=copytree(obj.c);  
+        }
+    }
+    private static btNode copytree(btNode t){      // copytree determines the order things are printed in
+                                                   // copytree does an inorder iteration
       btNode root=null;
       if (t!=null){
         root=new btNode();
@@ -44,15 +52,11 @@ public class Intcoll6 {
       }
       return root;
     }
-    
-    public void copy(Intcoll6 obj){
-        if (this!=obj){
-            this.howMany=obj.howMany;
-            c=copytree(obj.c);  
-        }
-    }
    
-    public void insert(int i){
+    public void insert(int i){       //searches the collection for int i using the belongs method, if belongs returns 
+                                     //true then insert does nothing. If belongs returns false then i is put into the 
+                                     //collection after the last non-zero value
+                                     //as a void function insert(int i) returns nothing
         if (i>0){
             btNode pred=null, p=c;
             while ((p!=null)&&(p.info!=i)){
@@ -72,8 +76,12 @@ public class Intcoll6 {
         }
     }
 
-    public void omit(int i){
-        if((i>0) && (this.belongs(i))){
+    public void omit(int i){                    //searches the collection for int i, if i is not found in the collection then
+                                                //nothing happens. if i is found then it is removed from the collection
+                                                //as a void function omit(int i) returns nothing
+       
+        if((i>0) && (this.belongs(i))){         //non zero and is in collection check
+            howMany--;
             btNode pred = null;
             btNode p = c;
             while( (p!=null) && (p.info!=i) ){  // assign p to the btNode with i
@@ -105,22 +113,29 @@ public class Intcoll6 {
         }
     }
 
-    public boolean belongs(int i){
+    public boolean belongs(int i){     //searches the collection for int i, returns true if found in collection and false 
+                                       //if not found
         btNode p=c;
-        while ((p!=null)&&(p.info!=i)){
+        while ((p!=null)&&(p.info!=i)){ //iterate through collection
             if (p.info>i) p=p.left;
             else p=p.right;
         }
       return (p!=null); 
     }
 
-   public int getHowmany() {
+   public int getHowmany() {         //interates over the c array and counts the items that are not zero, this will
+                                     //return the amount of viable values in the collection, this replaces a length-
+                                     //type method since the size of the array might not be the same as the true size
+                                     //of the collection, see empty param instantiation
        return howMany;
    }
    
    
     
-    public boolean equals(Intcoll6 obj){
+    public boolean equals(Intcoll6 obj){     //compares another Intcoll object with this Intcoll object
+                                             //if every int in both Intcoll arrays are the same, the items have the same
+                                             //collection contents up until the SENTINAL and order does NOT matter,
+                                             //then equals returns true, if not then equals returns false
         boolean result = (this.howMany == obj.howMany);
         if(result){
             // traverse and compare each node of the array
@@ -140,16 +155,17 @@ public class Intcoll6 {
         }
         return result;
     }
-    public static int toarray(btNode t, int[] a, int i){                        // toarray takes a btNode, int array, and staring index and fills
-        int num_nodes = 0;                                                      // the array starting at specified index and returns the number of 
-        if( t != null){                                                         // nodes in the btree
+    public static int toarray(btNode t, int[] a, int i){             // toarray takes a btNode, int array, and staring index and fills
+        int num_nodes = 0;                                           // the array starting at specified index and returns the number of 
+        if( t != null){                                              // nodes in the btree
             num_nodes = toarray(t.left,a,i);
             a[num_nodes+i] = t.info;
             num_nodes = num_nodes+1+toarray(t.right, a, num_nodes+i+1);
         }
         return num_nodes;
     }
-    public static void equalstree(int[] ray, int i, btNode t){
+    public static void equalstree(int[] ray, int i, btNode t){          // why did I put equalstree here
+                                                                        // equalstree orders items into an array, but toarray does this
         if( t != null){
             equalstree(ray, i, t.left);
             ray[i] = t.info;
@@ -157,8 +173,9 @@ public class Intcoll6 {
         }
     }
     
-    // btNode is described in assignment on canvas, most likely
-    // inner class btNode ***insert***
+    
+   
+   // inner class btNode
     private static class btNode{
        int info; 
        btNode left; 
@@ -176,4 +193,5 @@ public class Intcoll6 {
           right=null;
        }
    }
+   
 }
